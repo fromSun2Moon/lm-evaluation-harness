00de289e304cedc1088ad6f2763cc2c4cfa1ea96
fromSun2Moon <carpediem20@korea.ac.kr>
2023-07-15 18:29:41 +0900

    legal update

--- lm_eval/tasks/__init__.py
+++ lm_eval/tasks/__init__.py
@@ -57,6 +57,7 @@ from . import ko_translation
 from . import korquad
 from . import korunsmile
 from . import kohatespeech
+from . import legal_test
 from . import kold
 from . import toxigen
 from . import crowspairs
@@ -345,6 +346,9 @@ TASK_REGISTRY = {
     "kohatespeech":kohatespeech.HateSpeech,
     "kohatespeech_gen_bias":kohatespeech.GenderBias,
     "kohatespeech_apeach":kohatespeech.Apeach,
+    "kolegal_legalcase":legal_test.LegalBinary,
+    "kolegal_civilcase":legal_test.LJPCivil,
+    "kolegal_criminalcase":legal_test.LJPCriminal,
     **xcopa.construct_tasks(),
     **bigbench.create_all_tasks(),
     **xstorycloze.create_all_tasks(),
